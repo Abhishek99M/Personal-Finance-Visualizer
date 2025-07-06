dfb59651cc8d28936abbdd2b89f124af7ab065fc
Abhishek Kumar <110620295+Abhishek99M@users.noreply.github.com>
2025-07-07 02:34:08 +0530

    Resolve the Error again

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,17 @@
-import path from "path"
-import { fileURLToPath } from "url"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+    alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },)
+  },
+});

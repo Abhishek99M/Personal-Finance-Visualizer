a785e2ad7fd3debf151e24c3fe71463a15dfbb5a
Abhishek Kumar <110620295+Abhishek99M@users.noreply.github.com>
2025-07-07 02:49:11 +0530

    Resolve

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -14,4 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
